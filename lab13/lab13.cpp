d5b3a0a2e85defd0417c648533ee74bee162ba15
termine1 <gsafdq.tn.qe12@gmail.com>
2026-03-29 16:02:37 +0700

    1 ЗАДАНИЕ Код +- похож на код с лекции Создаю константу eps приближенную к 0 Создаю функцию для решения уравнения Выписываю все элементарные преобразования Создаю массив для отслеживания перестановок столбцов L
    В лекции нахожу код для нахождение ранга матрицы(прямой ход) и начинаю +- также переделывать:
    Создаю цикл который идет до конца столбцов или до макс ранга
    v = строка с потенциальным вед.элементом
    u = столбец с потен.ведущим элементом
    в внутреннем цикле:
    Ищем максимальные по модулю элемент в матрице
    и записываем в v и u
    ВАЖНО( столбцы и строки начинаются с 0 )!
    
    Если элемент не найден, то пропускаем строку и столбец этот

--- lab13/lab13.cpp
+++ lab13/lab13.cpp
@@ -1,5 +1,51 @@
 ﻿
 #include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+const float eps = 0.000001;
+
+
+
+int gaus(int M, int N, float** A, float* X) {
+// Элементарные преобразования
+// 1) Перестановка строк(уравн) и столбцов(переменных)
+// 2)  Вычитание из i строки k строки, умноженной на с
+// 3) Деление i строки на a[i][i]
+    int i, j, k, v, u, r;
+    float c, z;
+
+    int* L = new int[N];
+    for (int i = 0; i < N; i++) L[i] = i;
+
+    r = 0;
+
+    // прямой ход
+    for (int i = 0; i < N && r < M; i++) {
+        v = r; u = i; 
+
+        for (int j = r; j < M; j++) {
+            for (int k = i; k < N; k++) {
+                if (fabs(A[j][k]) > fabs(A[v][u])) {
+                    v = j;
+                    u = k;
+                }
+
+            }
+        }
+        if (fabs(A[v][u]) < eps) {
+            //не найден
+            continue;
+        }
+    }
+
+
+}
+
+
+
 
 int main()
 {
